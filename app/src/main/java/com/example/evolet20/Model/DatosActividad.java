9ca9db96980a74fd8211d8f35bfedf21f5a6b5c8
raqcav <raqcav@alu.iesserpis.org>
2024-05-29 18:35:58 +0200

    creado modelo entrenamiento y datos_actividad

---
+++ app/src/main/java/com/example/evolet20/Model/DatosActividad.java
@@ -0,0 +1,11 @@
+package com.example.evolet20.Model;
+
+public class DatosActividad {
+    public String idEntrenamiento;
+    public double tiempoTotal;
+    public double distanciaTotal;
+    public double velocidadMedia;
+    public int fc;
+    public String comentario;
+    public boolean realizado;
+}
---
+++ app/src/main/java/com/example/evolet20/Model/Entrenamiento.java
@@ -0,0 +1,13 @@
+package com.example.evolet20.Model;
+
+public class Entrenamiento {
+    public String id;
+    public String idDeportista;
+    public String idEntrenador;
+    public String fecha;
+    public String sesion;
+    public double km;
+    public int nSemana;
+
+
+}
